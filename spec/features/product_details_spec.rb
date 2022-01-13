0a82523d69792c461bde105aecfd4531b34919a9
Tarin Kazi <tarin.kazi.afrin@gmail.com>
2022-01-13 02:32:25 -0500

    product details feature

---
+++ spec/features/product_details_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.feature "Visitor navigates to details of single product", type: :feature, js: true do
+
+   # SETUP
+   before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+
+  scenario "User can see all details of selected product" do
+    visit root_path
+    
+    find_link("Details", match: :first).trigger("click")
+    expect(page).to have_css 'section.products-show'
+    save_screenshot 
+     puts page.html
+
+  end
+
+end
